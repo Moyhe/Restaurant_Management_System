659fd69c072a8d6dfdacb8ce6873dfaade8ccb04
Mohye Mahmoud <Mohye>
2023-10-07 22:24:14 +0300

    adding front end pages for the project, cart, checkout, contact, about, register, login, menu, service, error

--- public/index.php
+++ public/index.php
@@ -10,12 +10,14 @@ require BASE_PATH.'vendor/autoload.php';
 require BASE_PATH.'Core/functions.php';
 require BASE_PATH.'bootstrap.php';
 
+
 $router = new Router();
 
 require BASE_PATH.'routes.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
 
 try {
     $router->route($uri, $method);
